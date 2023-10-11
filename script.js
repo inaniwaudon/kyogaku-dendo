d093c39e90f0ccebcb8eb07ec9c06dd63734452f
inaniwaudon <w.yuto0916@gmail.com>
2023-10-11 13:05:34 +0900

    improve access counter

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ document.body.addEventListener("copy", (e) => {
   e.preventDefault();
   alert("コピーも禁止です!!");
 });
+
+const fetchAccessCounter = async () => {
+  const url = "https://renbai-counter.yokohama.dev/";
+  const request = await fetch(url);
+  return (await request.json()).count;
+};
+
+window.addEventListener("load", async () => {
+  const span = document.querySelector("#access-counter-no");
+  const count = await fetchAccessCounter();
+  const countStr = ("000" + count).slice(-6);
+  for (let i = 0; i < 6; i++) {
+    const digit = countStr[i];
+    console.log(digit);
+    const img = document.createElement("img");
+    img.src = `digits/${digit}.gif`;
+    span.appendChild(img);
+  }
+});
