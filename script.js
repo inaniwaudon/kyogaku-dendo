c5126ca647bda46e147189dac6d9aa79dfba71a4
inaniwaudon <w.yuto0916@gmail.com>
2023-10-28 22:29:45 +0900

    ディレクトリ構成の変更

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
+// アクセスカウンタのフェッチ
 const fetchAccessCounter = async () => {
   const url = "https://renbai-counter.yokohama.dev/";
-  const request = await fetch(url);
-  return (await request.json()).count;
+  try {
+    const request = await fetch(url);
+    return (await request.json()).count;
+  } catch {
+    return null;
+  }
 };
 
-window.addEventListener("load", async () => {
+// トップページのみ、アクセスカウンタを表示
+const displayAccessCounter = async () => {
   const span = document.querySelector("#access-counter-no");
-  const count = await fetchAccessCounter();
-  const countStr = ("000" + count).slice(-6);
+  if (!span) {
+    return;
+  }
+  let count = await fetchAccessCounter();
+  if (count === null) {
+    count = 0;
+  }
+  const countStr = ("00000" + count).slice(-6);
   for (let i = 0; i < 6; i++) {
     const digit = countStr[i];
     console.log(digit);
     const img = document.createElement("img");
-    img.src = `digits/${digit}.gif`;
+    img.src = `img/digits/${digit}.gif`;
     span.appendChild(img);
   }
+};
 
+// 共通ナビゲーションの表示
+const displayNavigation = () => {
+  const navHtml = `<h2>驚額の殿堂</h2>
+<ul>
+  <li><a href="/">トップ</a></li>
+  <li><a href="/maps">地図</a></li>
+  <li><a href="/poster">ポスター</a></li>
+  <li>んぽたその部屋（工事中）</li>
+</ul>
+<a href="https://sites.google.com/view/happy-busy/">
+  <img src="img/jikan.webp" alt="時間ねぇー" />
+</a>`;
+  const nav = document.getElementById("nav");
+  nav.innerHTML = navHtml;
+};
+
+window.addEventListener("load", async () => {
+  displayAccessCounter();
+  displayNavigation();
+
+  // 右クリック・コピーの禁止
   document.body.addEventListener("contextmenu", (e) => {
     e.preventDefault();
     alert("右クリックは禁止です!!");
   });
-
   document.body.addEventListener("copy", (e) => {
     e.preventDefault();
     alert("コピーも禁止です!!");
